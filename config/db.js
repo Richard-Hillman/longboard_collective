b947c06394a3861f47941c2fa48e97a944d51e16
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-02-01 15:39:41 -0800

    added to connectDB useNewUrlParser useFindAndModify UseUnifiedTopology useCreateIndex

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+const { parse } = require('url');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {
-            useUnifiedTopology: true
+        await mongoose.connect(db, 
+            {useCreateIndex: true,
+                useNewUrlParser: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true
         });
 
         console.log('mongoDB Connected...');
