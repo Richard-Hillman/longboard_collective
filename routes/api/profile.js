2e11f851e703129644405ddcef366191f5760d71
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-04-13 21:44:49 -0700

    added profile route. Can add proifle information. Tested on postman.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -50,6 +50,67 @@ async (req, res) => {
         if(!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
+        const {
+            company,
+            website,
+            location,
+            status,
+            skills,
+            bio,
+            youtube,
+            twitter,
+            facebook,
+            linkedin,
+            instagram
+        } = req.body;
+
+// Build Profile Object 
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if(company) profileFields.company = company;
+        if(website) profileFields.website = website;
+        if(location) profileFields.location = location;
+        if(status) profileFields.status = status;
+        if(skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+        }; 
+        if(bio) profileFields.bio = bio;
+   
+// build social objects 
+        profileFields.social = {}
+        if(youtube) profileFields.social.youtube = youtube;
+        if(twitter) profileFields.social.twitter = twitter;
+        if(facebook) profileFields.social.facebook = facebook;
+        if(linkedin) profileFields.social.linkedin = linkedin;
+        if(instagram) profileFields.social.instagram = instagram;
+
+        // TRY----------------------------         ----------------------------------------------
+        try{ 
+            let profile = await Profile.findOne({ user: req.user.id });
+
+            if(profile) {
+                // update
+                profile = await Profile.findOneAndUpdate(
+                    { user: req.user.id},
+                    { $set: profileFields },
+                    { new: true } 
+                );
+
+                return res.json(profile);
+            }
+
+            // create
+            profile = new Profile(profileFields);
+
+            await profile.save()
+            res.json(profile);
+
+        } catch {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+        // CATCH---------------------------        -----------------------------------------------
     }
 );
 
