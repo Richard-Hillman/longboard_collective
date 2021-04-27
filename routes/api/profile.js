14a9dc69a95f50d961bd93626475520fe50c1e66
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-04-26 21:05:42 -0700

    made create and update route made get all profiles route made get one user profile by id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -114,4 +114,47 @@ async (req, res) => {
     }
 );
 
+//  Gets ALL profiles--------------------------------------------------------------------------------------
+
+    // @route GET api/profile
+    // @desc GET all profiles
+    // @access Public
+
+    router.get('/', async (req, res) => {
+        try {
+            const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+            res.json(profiles);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
+
+//  Gets Users profile--------------------------------------------------------------------------------------
+
+    // @route GET api/profile/user/:user_id
+    // @desc GET profile by user ID
+    // @access Public
+
+    router.get('/user/:user_id', async (req, res) => {
+        try {
+            const profile = await Profile.findOne({
+                user: req.params.user_id
+            }).populate('user', ['name', 'avatar']);
+
+            if(!profile)
+                return res.status(400).json({ msg: 'Cannot Find Profile' });
+
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            if(err.kind == 'ObjectId') {
+                return res.status(400).json({ msg: 'Profile not Found' });
+            }
+            res.status(500).send('Server Error');
+        }
+    }
+);
+
 module.exports = router;
