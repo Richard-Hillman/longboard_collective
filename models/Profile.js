365442d68244b805485ee7c1f8a2c00ddf3b7ad4
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-04-26 22:26:20 -0700

    error handling for inserting object to experience in user profile not working

--- models/Profile.js
+++ models/Profile.js
@@ -40,7 +40,7 @@ const ProfileSchema = new mongoose.Schema({
           },
           from: {
             type: Date,
-            required: true
+            required: false
           },
           to: {
             type: Date
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,7 +157,7 @@ async (req, res) => {
     }
 );
 
-//  Gets Users profile by ID--------------------------------------------------------------------------------------
+//  Delete a user--------------------------------------------------------------------------------------
 
     // @route DELETE api/profile
     // @desc Delete profile and user and posts 
@@ -178,4 +178,65 @@ async (req, res) => {
     }
 );
 
+//  Gets Users profile by ID--------------------------------------------------------------------------------------
+
+    // @route PUT api/profile/experience
+    // @desc Add profile experience 
+    // @access Private
+
+    router.put('/experience',
+    [
+        auth,
+        [             
+            check('title, Title is required')
+                .not()
+                .isEmpty(),
+            check('company, Company is required')
+                .not()
+                .isEmpty()
+        ]
+    ],
+        async (req, res) => {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const {
+                title, 
+                company,
+                location,
+                from,
+                to,
+                current,
+                description
+            } = req.body;
+
+            const newExp = {
+                title,
+                company,
+                location,
+                from,
+                to,
+                current,
+                description
+            }
+
+            try {
+                const profile = await Profile.findOne({ user: req.user.id });
+                
+                // unshift pushes new exp to the beginning rather than end of array
+                profile.experience.unshift(newExp);
+
+                await profile.save();
+
+                res.json(profile);
+            } catch (err) {
+                console.error(err.message);
+                res.status(500).send('Server Error');
+            }
+    }
+);
+
+    // --------------------------------------------------------------------------------------
 module.exports = router;
